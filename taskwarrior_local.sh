5ec1913466398e5b249d0793ccb750ec0a47b27b
Janto <janto@team.uber.space>
2020-04-18 17:46:25 +0200

    :rotating_light: lint source

--- taskwarrior_local.sh
+++ taskwarrior_local.sh
@@ -7,53 +7,50 @@
 #
 # https://taskwarrior.org/
 
-mk-task-db ()
-{
-  # start a local ($PWD) tasklist
-  if [ -e '.taskrc' ]; then
-    echo '[ERROR] a Taskwarrior config already exists here.'
-  else
-    # include global settings
-    if [ -e "$HOME/.taskrc" ]; then
-      # shellcheck disable=SC2016
-      echo 'include $HOME/.taskrc' > .taskrc
-    fi
-    # use local DB
-    echo "data.location=$(pwd)/.taskwarrior" >> .taskrc
-    # create local DB if needed
-    if [ ! -e '.taskwarrior' ]; then
-      mkdir .taskwarrior
-    fi
-  fi
+mk-task-db() {
+	# start a local ($PWD) tasklist
+	if [ -e '.taskrc' ]; then
+		echo '[ERROR] a Taskwarrior config already exists here.'
+	else
+		# include global settings
+		if [ -e "$HOME/.taskrc" ]; then
+			# shellcheck disable=SC2016
+			echo 'include $HOME/.taskrc' > .taskrc
+		fi
+		# use local DB
+		echo "data.location=$(pwd)/.taskwarrior" >> .taskrc
+		# create local DB if needed
+		if [ ! -e '.taskwarrior' ]; then
+			mkdir .taskwarrior
+		fi
+	fi
 }
 
-mk-task-project ()
-{
-  # set local default project name
-  # ARG1: default project name
-  #
-  if [ -e '.taskrc' ]; then
-    echo '[ERROR] a Taskwarrior config already exists here.'
-    return 1
-  fi
-  local pro="${1:-$(basename $(pwd))}"
-  # include global settings
-  if [ -e "$HOME/.taskrc" ]; then
-    # shellcheck disable=SC2016
-    echo "include $HOME/.taskrc" > .taskrc
-  fi
-  # set local project oerride
-  echo "default.project=$pro" >> .taskrc
+mk-task-project() {
+	# set local default project name
+	# ARG1: default project name
+	#
+	if [ -e '.taskrc' ]; then
+		echo '[ERROR] a Taskwarrior config already exists here.'
+		return 1
+	fi
+	local pro="${1:-$(basename $(pwd))}"
+	# include global settings
+	if [ -e "$HOME/.taskrc" ]; then
+		# shellcheck disable=SC2016
+		echo "include $HOME/.taskrc" > .taskrc
+	fi
+	# set local project oerride
+	echo "default.project=$pro" >> .taskrc
 }
 
-local-task ()
-{
-  # use local $PWD tasklist, instead of global
-  if [[ "$PWD" != "$HOME" && -e ".taskrc" ]]; then
-    task rc:.taskrc "$@"
-  else
-    task "$@"
-  fi
+local-task() {
+	# use local $PWD tasklist, instead of global
+	if [[ "$PWD" != "$HOME" && -e ".taskrc" ]]; then
+		task rc:.taskrc "$@"
+	else
+		task "$@"
+	fi
 }
 
 alias lt="local-task"
